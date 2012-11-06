2f1aa7e0e4d657c2366d5de0c02949839b7ae702
lkalli <laura.kalli@helsinki.fi>
2012-11-07 00:14:31 +0200

    dummy-tiedosto

---
+++ javalabra/src/NetbeansKoe.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author laura
+ */
+public class NetbeansKoe {
+    //public static void main(String[] args) {
+       // System.out.println("asdf");
+        
+        //toinen pushaustesti, nyt suoraan netbeansista
+    }
+
