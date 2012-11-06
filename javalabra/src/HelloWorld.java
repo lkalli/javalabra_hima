8c17d9e408a14ee1814d2fdb229e259fd49f618c
lkalli <laura.kalli@helsinki.fi>
2012-11-06 23:39:55 +0200

    kokeillaan meniskö netbeansista githubiin

---
+++ javalabra/src/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author laura
+ */
+public class HelloWorld {
+    
+    public static void main (String[] args) {
+        System.out.println("Hei maailma!");
+        //testataan meneekö tää nyt lopulta githubiin
+}
+}
