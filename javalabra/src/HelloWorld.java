6cc0170cd8645cb86fbaf42c0bceb878c25298c4
lkalli <laura.kalli@helsinki.fi>
2012-11-07 02:01:07 +0200

    testiluokka

--- javalabra/src/HelloWorld.java
+++ javalabra/src/HelloWorld.java
@@ -9,8 +9,13 @@
  */
 public class HelloWorld {
     
+    public HelloWorld () {
+        
+    }
     public static void main (String[] args) {
         System.out.println("Hei maailma!");
         //testataan meneekö tää nyt lopulta githubiin
+        
+        
 }
 }
